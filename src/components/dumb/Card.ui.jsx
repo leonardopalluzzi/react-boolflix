e26dae8af9ef3e798f6e2f95cd28aea9717125cb
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-04 20:17:43 +0200

    refactor Card components to display ratings as stars and improve layout consistency

--- src/components/dumb/Card.ui.jsx
+++ src/components/dumb/Card.ui.jsx
@@ -5,12 +5,11 @@ export default function CardUi({ movie }) {
     const imgPath = 'https://image.tmdb.org/t/p/w185'
 
     let language = movie.original_language;
+    let rating = Math.ceil(movie.vote_average / 2)
 
     if (language == 'en') {
         language = 'uk'
     }
-
-
 
     return (
         <>
@@ -24,7 +23,7 @@ export default function CardUi({ movie }) {
                             <li>{movie.title}</li>
                             <li>{movie.original_title}</li>
                             <li><img className="card_img" src={`https://www.worldometers.info//img/flags/small/tn_${language}-flag.gif`} alt="language" /></li>
-                            <li>{movie.vote_average}</li>
+                            <li className="d-flex">{rating == 0 ? 'No rating' : Array.from(Array(rating), () => <i class="bi bi-star-fill text-warning"></i>)}</li>
                         </ul>
                     </div>
                 </div>
--- src/components/dumb/CardSerie.ui.jsx
+++ src/components/dumb/CardSerie.ui.jsx
@@ -18,8 +18,8 @@ export default function CardSerieUi({ serie }) {
     return (
         <>
             <div className="container">
-                <div className="card w-50 text-center m-auto">
-                    <div className="card-header">
+                <div className="card w-50 m-auto">
+                    <div className="card-header text-center">
                         <img src={`${imgPath}${serie.poster_path}`} alt="" />
                     </div>
                     <div className="card-body">
@@ -27,7 +27,7 @@ export default function CardSerieUi({ serie }) {
                             <li>{serie.name}</li>
                             <li>{serie.original_name}</li>
                             <li><img className="card_img" src={`https://www.worldometers.info//img/flags/small/tn_${language}-flag.gif`} alt="language" /></li>
-                            <li>{serie.vote_average}</li>
+                            <li>{Math.round(serie.vote_average / 2)}</li>
                         </ul>
                     </div>
                 </div>
