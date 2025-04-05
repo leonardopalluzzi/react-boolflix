d91e58072980150f59953616b66744675e96968d
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-05 03:32:17 +0200

    refactor Jumbo and List components for improved layout and responsiveness

--- src/components/dumb/List.ui.jsx
+++ src/components/dumb/List.ui.jsx
@@ -7,14 +7,22 @@ export default function ListUi({ movies, series }) {
         <>
             <div className="container">
                 <h1>movies</h1>
-                {movies.map(item => (
-                    <Card key={item.id} movie={item} />
-                ))}
+                <div className="container movies d-flex">
+
+                    {movies.map(item => (
+                        <Card key={item.id} movie={item} />
+                    ))}
+
+
+                </div >
                 <h1>Series</h1>
-                {series.map(item => (
-                    <CardSeries key={item.id} serie={item} />
-                ))}
-            </div >
+                <div className="container series d-flex">
+                    {series.map(item => (
+                        <CardSeries key={item.id} serie={item} />
+                    ))}
+                </div>
+            </div>
+
         </>
     )
 }
