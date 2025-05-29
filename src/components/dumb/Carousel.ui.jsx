e56cc6ce35cd628a9463a26b87495281122b29ad
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-05-29 23:56:26 +0200

    text center on carousel title

--- src/components/dumb/Carousel.ui.jsx
+++ src/components/dumb/Carousel.ui.jsx
@@ -19,7 +19,7 @@ export default function Carousel({ content, title }) {
     return (
         <>
             <div className="item_list py-4">
-                <h1>{title}</h1>
+                <h1 className="text-center">{title}</h1>
                 <div className="container-fluid d-flex justify-content-between py-4">
                     <button className="caro_btn" onClick={() => scroll('left')}><i class="bi bi-caret-left"></i></button>
                     <div ref={scrollRef} className="lists_scroll movies d-flex p-4">
